f2245138f54482e5071e5ebe6691368e00127314
Foster Azrael <62256436+siddshakya@users.noreply.github.com>
2024-04-27 17:03:25 +0545

    o

--- TrekkingCoding/Login.php
+++ TrekkingCoding/Login.php
@@ -7,19 +7,19 @@
     <body class="login">
         <?php
         if(isset($_POST["login"])){
-            $email = $_POST["email"];
+            $emailAddress = $_POST["emailAddress"];
             $password = $_POST["password"];
             require_once("database.php");
-            $sql = "SELECT * FROM user_form WHERE email = $email";
+            $sql = "SELECT * FROM user_form WHERE email = '$emailAddress'";
             $result = mysqli_query ($conn, $sql);
-            $username = mysqli_fetch_array($result, MYSQLI_ASSOC);
-            if($username){
-                if(password_verify($password, $username["password"])){
+            $user = mysqli_fetch_array($result, MYSQLI_ASSOC);
+            if($user){
+                if(password_verify($password, $user["password"])){
                     header("Location: Dashboard.html");
                     die();
                 }
             }else{
-                echo "<div class='alert'>email does not exist</div>";
+                echo "<div class='text-white bg-dark'>email or password does not match </div>";
             }
         }
         ?>
@@ -27,12 +27,12 @@
                 <form style="max-width: 400px;margin: auto"method="post">
                     <img class="mt-4 mb-4" src="Logoo1.png" height="170">
                     <h1 class="front-page">Sign in</h1>
-                    <input type="email" id="emailAddress" class="form-control mt-3 mb-3" name="email" placeholder="Email Address" required="required">
+                    <input type="email" id="emailAddress" class="form-control mt-3 mb-3" name="emailAddress" placeholder="Email Address" required="required">
                     <input type="password" id="Password"  class="form-control" name="password" placeholder="Password" required="required">
                     <div class="mt-3">                
-                        <button type="submit" value ="login" class="btn btn-primary btn-block" formaction="UserLogin.php" value="submit_2">User Login</button>
+                        <button type="submit" value ="login" name="login" class="btn btn-primary btn-block">User Login</button>
                         <a href="Register.php" class="btn btn-primary">Register</a>
-                        <button type="submit" class="btn btn-primary btn-block" formaction="AdminLogin.php" value="submit_1">Admin Login</button>
+                        <button type="submit-1" class="btn btn-primary btn-block" formaction="AdminLogin.php">Admin Login</button>
                     </div>
                 </form>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
